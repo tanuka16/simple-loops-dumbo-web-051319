8deccc874738c3e7f0a0e7d713513e39bab15715
Tanuka Das <das.tanuka@yahoo.com>
2019-04-24 22:58:30 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -20,19 +20,13 @@ end
 end
 
 
-
-
-
-
-
-
-
-
-
-
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
+  7.times do
+    puts phrase
+  end
   phrase = "Welcome to Flatiron School's Web Development Course!"
+
 
 end
 
